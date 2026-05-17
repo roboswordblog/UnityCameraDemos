5374432073f5b5de02c3ee2b05a149e85a235bc0
Robosword <32asingh@stratfordschools.net>
2026-05-17 09:58:29 -0700

    Create player.cs

---
+++ player.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    [SerializeField] float jumpForce = 7f;
+    private Rigidbody rb;
+
+    private float moveX;
+    private float moveZ;
+    private bool isGrounded = true;
+    public float sensitivity = 2F;
+    private float rotationX;
+    private float rotationY;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+
+    void Update()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveZ = Input.GetAxisRaw("Vertical");  
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            rotationY += mouseX * sensitivity;
+            rotationX -= mouseY * sensitivity;
+
+            transform.rotation = Quaternion.Euler(rotationX, rotationY, 0f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = (transform.right * moveX + transform.forward * moveZ).normalized;
+
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+        
+    }
+}
